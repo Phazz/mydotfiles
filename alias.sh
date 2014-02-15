c8c656426492e7f0925e25f310cc9129ec000a66
Pierre Lemperiere <pierre.lemperiere@gmail.com>
2014-02-16 00:19:13 +0400

    initial commit

---
+++ alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s $PWD/vim $HOME/.vim
+ln -s $PWD/vim/vimrc $HOME/.vimrc
+
+ln -s $PWD/zsh $HOME/.zsh
+ln -s $PWD/zsh/zshrc $HOME/.zshrc
+
+ln -s $PWD/tmux/tmuxconf $HOME/.tmux.conf
